7e3ab68c70c5d867e9c8a60a1f60ec5370c15e1b
jizey <jizey@opera.com>
2022-07-21 16:21:46 +0800

    Revert "[ADX-8614] reuse bigint"
    This reverts commit 06cb622b9204d85d6081600f0f11975a9fe65419.

--- pool.go
+++ pool.go
@@ -1,9 +1,6 @@
 package ip2location
 
-import (
-	"math/big"
-	"sync"
-)
+import "sync"
 
 var fourBytesPool = sync.Pool{New: func() interface{} {
 	return make([]byte, 4)
@@ -18,26 +15,4 @@ func getFourBytes() []byte {
 func putFourBytes(data []byte) {
 	copy(data, emptyFourBytes)
 	fourBytesPool.Put(data)
-}
-
-var (
-	proxy = bigIntProxy{sync.Pool{New: func() interface{} {
-		return big.NewInt(0)
-	}}}
-	bigInt1 = big.NewInt(1)
-)
-
-type bigIntProxy struct {
-	pool sync.Pool
-}
-
-func (p *bigIntProxy) NewInt(num int64) *big.Int {
-	return p.pool.Get().(*big.Int).SetInt64(num)
-}
-
-func (p *bigIntProxy) RecycleInt(i *big.Int) {
-	if i == nil {
-		return
-	}
-	p.pool.Put(i)
 }
